c6a85f7071f98f35b0d842680ce9b048607871e2
Patriziano <onahpatrick75@gmail.com>
2023-10-03 14:50:35 -0700

    A ruby script that outputs: [SENDER],[RECEIVER],[FLAGS] from a textme app

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\]\s\[to:([a-zA-Z0-9+]+)\]\s\[flags:([0-9:-]+)\]/).join
+puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]+)\]\s\[to:([a-zA-Z0-9+]+)\]\s\[flags:([0-9:-]+)\]/).join(",")
