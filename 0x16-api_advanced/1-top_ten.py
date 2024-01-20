9298c8b7f84ce36a45551240e6d562fab1771f4c
Patriziano <onahpatrick75@gmail.com>
2024-01-20 14:54:22 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
 """a function that queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit."""
-
+titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    """This function gets first 10 comments"""
+    apiUrl = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    response = requests.get(url, headers=headers)
+    headers = {'User-Agent': 'MyBot/1.0'}
+    res = requests.get(apiUrl, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
+    if res.status_code == 200:
+        data = res.json()
+        posts = data['data']['children']
 
-        if posts:
-            for post in posts:
-                title = post['data']['title']
-                print(title)
-        else:
-            print("No posts found.")
-    elif response.status_code == 404:
-        print("Invalid subreddit. Please provide a valid subreddit.")
+        for post in posts:
+            title = post['data']['title']
+            print(title)
     else:
-        print("Error fetching data from Reddit API.")
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-
+        print('None')
